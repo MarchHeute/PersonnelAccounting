015740bf89429b73b777ac1ff17ae67ac89cba6b
Valentin <Vell0505189970sl@gmail.com>
2022-06-20 11:39:53 +0300

    Added viewmodel

---
+++ ViewModel/PersonnelAccountingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace PersonnelAccounting.ViewModel
+{
+    public class PersonnelAccountingViewModel
+    {
+        public IMongoDatabase? MongoDatabase { get; private set; }
+
+        public PersonnelAccountingViewModel()
+        {
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://m1adow:<password>@personnelaccounting.mnxlj.mongodb.net/?retryWrites=true&w=majority");
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            MongoDatabase = client.GetDatabase("PersonnelAccounting");
+        }
+    }
+}
